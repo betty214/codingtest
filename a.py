c5234f966b0ef9048edd8626c9aba28086b24d33
betty214 <bettyjang@gmail.com>
2020-07-14 15:45:54 +0900

    7.14_example2

--- a.py
+++ a.py
@@ -1,2 +1,10 @@
-str="hello"
-print("ll" in str)
+year = int(input())
+
+if year%400==0:
+	print("Leap Year")
+elif year%100==0:
+	print("Not Leap Year")
+elif year%4==0:
+	print("Leap Year")
+else:
+	print("Not Leap Year")
