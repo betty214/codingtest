7badac090723fc1cf383aa5e297c9f9771388b27
betty214 <bettyjang@gmail.com>
2020-07-14 16:35:33 +0900

    7.14_example5

--- a.py
+++ a.py
@@ -1,3 +1,17 @@
+import math
 n = int(input())
-for i in range(n):
-	print ("*"*(n-i))
+n2=int(math.sqrt(n))
+flag=0
+
+for i in range(n2+1):
+	if i==0 or i==1:
+		continue
+	elif (n//i)*i==n:
+		flag=1
+		break
+	
+		
+if flag==0:
+	print(True)
+else:
+	print(False)
