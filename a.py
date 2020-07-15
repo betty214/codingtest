5acc211a1784894d7f65e9b9de1656c3d60512da
betty214 <bettyjang@gmail.com>
2020-07-15 16:09:16 +0900

    7.15_example4_recursive

--- a.py
+++ a.py
@@ -1,20 +1,12 @@
-x=list(map(int,input().split(" ")))
-f=[0 for _ in range(len(x))]
-count=0
+a,b =map(int,input().split(" "))
 
-for i in range(len(x)):
-	if i==0:
-		f[0]+=1
+
+def bi(a,b):
+	if b==0:
+		return 1
+	elif a==b:
+		return 1
 	else:
-		count=0
-		flag=True
-		for j in range(i):
-			if x[j]==x[i]:
-				f[j]+=1
-				flag=False
-				break
-		if flag==True:
-			f[i]+=1
-			
-answer=f.index(max(f))
-print(x[answer])
+		return bi(a-1,b-1)+bi(a-1,b)
+
+print(bi(a,b))
