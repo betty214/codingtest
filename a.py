70715bdb02d10867a63e6a2c6acb5761db8d28d3
betty214 <bettyjang@gmail.com>
2020-07-15 16:08:17 +0900

    7.15_example3_myanswer

--- a.py
+++ a.py
@@ -1,6 +1,20 @@
-input_list=[3.3, 4.4, 5.5, 6.6, 7.7, 8.8]
-answer_list=[0,0,0,0,0,0]
-for i in range(len(input_list)):
-	num=int(input_list[i])
-	answer_list[i]=num
-print(answer_list)
+x=list(map(int,input().split(" ")))
+f=[0 for _ in range(len(x))]
+count=0
+
+for i in range(len(x)):
+	if i==0:
+		f[0]+=1
+	else:
+		count=0
+		flag=True
+		for j in range(i):
+			if x[j]==x[i]:
+				f[j]+=1
+				flag=False
+				break
+		if flag==True:
+			f[i]+=1
+			
+answer=f.index(max(f))
+print(x[answer])
