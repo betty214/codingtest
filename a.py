a6c5bc25765b191ebd9fcd4f17e685b1d46db0fa
betty214 <bettyjang@gmail.com>
2020-07-16 11:29:08 +0900

    7.16_example2

--- a.py
+++ a.py
@@ -1,13 +1,23 @@
 n = int(input())
-cache=[-1 for i in range(21)]
+cachet=[-1 for i in range(21)]
 def t(n):
+	if n==1:
+		return 1
+	if n==2 :
+		return 2
+	if cachet[n]!=-1:
+		return cachet[n]
+	else:
+		return t(n-1)+t(n-2)
+	
+def d(n):
 	if n==1:
 		return 1
 	if n==2:
 		return 2
-	if cache[n]!=-1:
-		return cache[n]
-	else:
-		return t(n-1)+t(n-2)
+	elif n%2==1:
+		return t((n)//2)
+	elif n%2==0:
+		return t((n//2)-1) + t((n)//2)
 
-print(t(n))
+print(t(n)-d(n))
