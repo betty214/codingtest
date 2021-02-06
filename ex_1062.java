347e5ff6448a9206102e0520c954c0fcae8ca0dc
betty214 <50865204+betty214@users.noreply.github.com>
2021-02-07 00:40:33 +0900

    백준 완전탐색(미완3)

--- ex_1062.java
+++ ex_1062.java
@@ -3,17 +3,18 @@ package backjun;
 import java.util.Scanner;
 
 public class ex_1062 {
+	public static char save[] = new char[10];
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 	    int n = scan.nextInt();
 	    int m = scan.nextInt();
 	    String letter[] = new String[n];
 	    char spell[][] = new char[50][15];
-	    String sp[]=new String[50];
 	    int flag=0,f=0,max=0;
 	    for(int i=0;i<n;i++) {
 	    	letter[i]= scan.next();
 	    }
+	    
 	    for(int i=0;i<n;i++) {
 	    	for(int j=0;j<letter[i].length();j++) {
 	    		char y=letter[i].charAt(j);
@@ -25,42 +26,48 @@ public class ex_1062 {
 	    			}
 	    			if(f==0) {
 	    				spell[i][flag]=y;
-	    				String s=Character.toString(y);
-	    				sp[i].concat(s);
 		    			flag++;
 	    			}f=0;
 	    		}
 	    	}flag=0;
 	    }
-	    for(int i=0;i<n;i++) {
-	    	for(int j=0;j<spell[i].length;j++) {
-		    	System.out.print(sp[i]);
-	    	}
-	    }
+	    
 	    if(m<5) {
 	    	System.out.println(0);
 	    }else {
 	    	max=FindMax(n,m,spell);
 	    	System.out.println(max);
 	    }
-	}public static int FindMax(int n, int m,char s[][]) {
-		int flag=0,max=0;
-		int x=n-m;
-		char save[]=new char [m];
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<15;j++) {
-				char y = s[i][j];
-				for(int k=0;k<x;k++) {
-					if(y!=save[k]) {
-						save[flag]=y;
-						flag++;
+	}
+
+	public static int FindMax(int n, int m, char s[][]) {
+		for (int j = 1; j <= (m - n); j++) {
+			for (int i = 0; i < n; i++) {
+				CheckMax(j,s[i]);
+			}
+		}
+		/*
+		for(int i=0;i<10;i++) {
+			System.out.print(save[i]+" ");
+		}*/
+		return 1;
+	}
+	
+	public static void CheckMax(int length, char st[]) {
+		int flag=0,index=0;
+
+		System.out.println(st.length);
+		if (st.length == length) {
+			for(int i=0;i<length;i++) {
+				for(int j=0;j<10;j++) {
+					if(st[i]==save[j]) {
+						flag=1;
 					}
+				}if(flag==0) {
+					save[index]=st[i];
+					index++;
 				}
 			}
 		}
-		for(int i=0;i<m;i++) {
-			System.out.println(save[i]);
-		}
-		return max;
 	}
 }
