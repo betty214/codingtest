fe409afc641309e5633c5315acc88211db2dd2c9
betty214 <50865204+betty214@users.noreply.github.com>
2021-05-19 00:41:10 +0900

    joystick 2

--- Greedy/joy.java
+++ Greedy/joy.java
@@ -2,15 +2,63 @@ import java.util.Arrays;
 class Solution {
     public int solution(String name) {
         char[]temp=name.toCharArray();
-        int sum=0;
-        int move=temp.length-1;
-        for(int i=0;i<temp.length;i++){
-            int front = (int)(temp[i]-'A');
-            int back = (int)('Z'-temp[i]+1);
+        int[]visited=new int[temp.length];
+        int sum=0;int move=0;int now=0;
+        int next=0;int c=0;int flag=0;
+        while(c==0){
+            visited[now]=1;
+            int front = (int)(temp[next]-'A');
+            int back = (int)('Z'-temp[next]+1);
             sum+=Math.min(front,back);
-            move=Math.min(move, i+1)
+            if(flag==0){
+                int t=0;
+                for(int i=temp.length-1;i>0;i++){
+                    if(temp[i]!='A'&&visited[i]==0){
+                        t=i;
+                        break;
+                    }
+                }if(t==0){
+                        c=1;
+                }
+                if(next-now<now+temp.length-t){
+                    move+=next-now;
+                    now=next;
+                    for(int i=now;i<temp.length;i++){
+                        if(temp[i]!='A'&&visited[i]==0){
+                        next=i;
+                        break;
+                    }
+                    }
+                }else{
+                    flag=1;
+                    move+=temp.length-next+now+1;
+                now=next;
+                for(int i=next;i>0;i--){
+                    if(temp[i]!='A'&&visited[i]==0){
+                        next=i;
+                        break;
+                    }else if(temp[i]!='A'&&visited[i]==1){
+                        c=1;
+                        break;
+                    }
+                }
+                }
+            }else{
+                move+=temp.length-next+now+1;
+                now=next;
+                for(int i=next;i>0;i--){
+                    if(temp[i]!='A'&&visited[i]==0){
+                        next=i;
+                        break;
+                    }else if(temp[i]!='A'&&visited[i]==1){
+                        c=1;
+                        break;
+                    }
+                }
+            }
         }
-        int answer = sum;
+        System.out.println(move);
+        int answer = sum+move;
         return answer;
     }
 }
