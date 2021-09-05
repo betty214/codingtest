8288f38ad1a9749e88ec8418be06cd856ad16de7
betty214 <bettyjang@ewhain.net>
2021-09-05 22:56:58 +0900

    chore : 9935 최종 수정

--- Stack/ex_9935.java
+++ Stack/ex_9935.java
@@ -2,19 +2,41 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class ex_9935 {
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str=br.readLine();
-        String bomb=br.readLine();
-        while(str.contains(bomb)){
-            str=str.replaceAll(bomb,"");
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        String str = br.readLine();
+        String bomb = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+            if (stack.size() >= bomb.length()) {
+                boolean flag = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    for (int j = 0; j < bomb.length(); j++) {
+                        stack.pop();
+                    }
+                }
+            }
         }
-        if(str==""){
+
+        for(char ch : stack) {
+            sb.append(ch);
+        }
+        if(sb.length()==0){
             System.out.println("FRULA");
         }else{
-            System.out.println(str);
+            System.out.println(sb.toString());
         }
     }
 }
