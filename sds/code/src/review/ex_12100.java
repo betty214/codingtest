4c932347755e78b40fa2e87cbadb0bd37e06a090
challenger214 <bettyjang@ewhain.net>
2022-02-23 11:36:03 +0900

    삼성 기출

---
+++ sds/code/src/review/ex_12100.java
@@ -0,0 +1,126 @@
+package review;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class ex_12100 {
+    private static class Node{
+        int x;
+        int y;
+        int num;
+        public Node (int x,int y, int num){
+            this.x=x;
+            this.y=y;
+            this.num=num;
+        }
+    }
+    static int N,answer;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("code/src/input/12100.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[][]board=new int[N][N];
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        answer=0;
+        for(int i=0;i<4;i++){
+            DFS(board, 0, i);
+        }
+        System.out.println(answer);
+    }static void DFS(int [][]now, int depth, int dir){
+        if(depth==4){
+            return;
+        }
+        Stack<Node>s=new Stack<>();
+        boolean flag=true;
+        switch(dir){
+            case 0: //상
+                for(int i=0;i<N;i++){
+                    for(int j=0;j<N;j++){
+                        if(now[j][i]==0) continue;
+                        answer=Math.max(answer,now[j][i]);
+                        if(s.size()==0){
+                            s.add(new Node(j,i,now[j][i]));
+                        }else{
+                            int pre=s.peek().num;
+                            if(pre==now[j][i] && s.peek().y==i && flag) {
+                                s.peek().num*=2;
+                                flag=false;
+                            }
+                            else s.add(new Node(j,i,now[j][i]));
+                        }
+                    }flag=true;
+                }
+                break;
+            case 1: //하
+                for(int i=0;i<N;i++){
+                    for(int j=N-1;j>=0;j--){
+                        if(now[j][i]==0) continue;
+                        answer=Math.max(answer,now[j][i]);
+                        if(s.size()==0){
+                            s.add(new Node(j,i,now[j][i]));
+                        }else{
+                            int pre=s.peek().num;
+                            if(pre==now[j][i] && s.peek().y==i && flag) {
+                                s.peek().num*=2;
+                                flag=false;
+                            }
+                            else s.add(new Node(j,i,now[j][i]));
+                        }
+                    }flag=true;
+                }
+                break;
+            case 2: //좌
+                for(int i=0;i<N;i++){
+                    for(int j=0;j<N;j++){
+                        if(now[i][j]==0) continue;
+                        answer=Math.max(answer,now[i][j]);
+                        if(s.size()==0){
+                            s.add(new Node(i,j,now[i][j]));
+                        }else{
+                            int pre=s.peek().num;
+                            if(pre==now[i][j] && s.peek().x==i && flag) {
+                                s.peek().num*=2;
+                                flag=false;
+                            }
+                            else s.add(new Node(i,j,now[i][j]));
+                        }
+                    }flag=true;
+                }
+                break;
+            case 3: //우
+                for(int i=0;i<N;i++){
+                    for(int j=N-1;j>=0;j--){
+                        if(now[i][j]==0) continue;
+                        answer=Math.max(answer,now[i][j]);
+                        if(s.size()==0){
+                            s.add(new Node(i,j,now[i][j]));
+                        }else {
+                            int pre = s.peek().num;
+                            if (pre == now[i][j] && s.peek().x == i && flag) {
+                                s.peek().num *= 2;
+                            } else s.add(new Node(i, j, now[i][j]));
+                        }
+                    }flag=true;
+                }
+                break;
+        }
+        int[][]temp=new int[N][N];
+        for(int i=0;i<s.size();i++){
+            Node n = s.pop();
+            temp[n.x][n.y]=n.num;
+        }
+        for(int i=0;i<4;i++){
+            DFS(temp, depth+1 ,i);
+        }
+
+    }
+}
---
+++ sds/code/src/review/ex_13460.java
@@ -0,0 +1,83 @@
+package review;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ex_13460 {
+    static int N,M,Ox,Oy,count;
+    static int[] dx={1,-1,0,0};
+    static int[] dy={0,0,1,-1};
+    static char[][]board;
+    static boolean flag;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("code/src/input/13460.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board=new char[N][M];
+        int Bx=0; int By=0; int Rx=0; int Ry=0; count=11;
+        flag=false;
+        for(int i=0;i<N;i++){
+            String input=br.readLine();
+            for(int j=0;j<M;j++){
+                board[i][j]=input.charAt(j);
+                if(board[i][j]=='R') {
+                    Rx=i;
+                    Ry=j;
+                }else if(board[i][j]=='B') {
+                    Bx=i;
+                    By=j;
+                }else if(board[i][j]=='O') {
+                    Ox=i;
+                    Oy=j;
+                }
+            }
+        }
+        DFS(Bx,By,Rx,Ry,0);
+        System.out.println(count+1);
+    }static void DFS(int Bx, int By, int Rx, int Ry, int depth){
+        if(!flag && depth<10){
+            for(int i=0;i<4;i++){
+                int cBx=Bx+dx[i];
+                int cBy=By+dy[i];
+                int cRx=Rx+dx[i];
+                int cRy=Ry+dy[i];
+                boolean bFlag=false;
+                boolean rFlag=false;
+                while(check(cBx,cBy)&& board[cBx][cBy]!='#'){
+                    if(isHoll(cBx,cBy)){
+                        bFlag=true;
+                        break;
+                    }
+                    cBx+=dx[i];
+                    cBy+=dy[i];
+                }
+                while(check(cRx,cRy)&& board[cRx][cRy]!='#'){
+                    if(isHoll(cRx,cRy)){
+                        rFlag=true;
+                        break;
+                    }
+                    cRx+=dx[i];
+                    cRy+=dy[i];
+                }
+                if(bFlag&&!rFlag) continue;
+                if(rFlag && !bFlag) {
+                    flag=true;
+                    count=Math.min(count, depth);
+                    break;
+                }
+                if(!flag && check(cBx,cBy) && check(cRx,cRy)) DFS(cBx, cBy, cRx, cRy, depth+1);
+            }
+        }
+    }static boolean check(int x, int y){
+        if(x>=N||y>=M||x<0||y<0) return false;
+        return true;
+    }static boolean isHoll(int x, int y){
+        if(x==Ox && y==Oy) return true;
+        return false;
+    }
+}
