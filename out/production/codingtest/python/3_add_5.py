e0efa6b012879ea5ea10f2c51a34f72d7b1cc4a8
challenger214 <bettyjang@ewhain.net>
2022-03-01 15:56:29 +0900

    TEST:gitignore 적용 확인

--- out/production/codingtest/python/3_add_5.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-sum=0
-
-for i in range(n+1):
-	if (i%3==0):
-		sum+=i
-	elif(i%5==0):
-		sum+=i
-print (sum)
--- out/production/codingtest/python/string_pop.py
+++
@@ -1,9 +0,0 @@
-s = input()
-j=0
-for i in range(0,len(s)):
-	if(i%2==0):
-		print(s[j],end="")
-		j+=1
-	else:
-		k=i//2+1
-		print(s[len(s)-k],end="")
