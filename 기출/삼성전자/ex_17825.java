060c9f1a50075fe4198221d089906273b0ef2047
challenger214 <bettyjang@ewhain.net>
2022-04-22 15:52:26 +0900

    삼성기출 : 주사위 윷놀이

--- 기출/삼성전자/ex_17825.java
+++ 기출/삼성전자/ex_17825.java
@@ -35,66 +35,73 @@ public class ex_17825 {
         }
         answer=Math.max(answer,sum);
         if(nindex>9){
+            System.out.println();
             return;
         }
 
-        for(int i=0;i<4;i++){
-            if(pos[i]==40) continue;
+        for(int i=0;i<2;i++){ //현재 상황에서 어떤 말을 움직일지 다 시험해보기
+            if(pos[i]==40) continue; //도착한 말은 움직이지 않기
+            //이전 상태 기록해 두기
             int pre=pos[i];
             int premod=mod[i];
-            int tmp=move(next[nindex],i,score[i], mod, pre);
-            if(tmp==0) continue;
+            int nextPos=move(next[nindex],premod, pre);//이동한 위치
+
+            //이동할 곳에 누가 있으면 이동 안함
+            boolean flag=true;
             for(int j=0;j<4;j++){
-                if(j!=i  && tmp!=pos[j]) continue;
+                if(nextPos==pos[j] && nextPos!=40) flag=false;
             }
+            if(flag && nextPos>0) {
+                //실제로 이동시킴
+                pos[i]=nextPos;
+                if(pos[i]!=40) score[i]+=pos[i];
+                //다른 루트를 타면 모드를 변경 시켜줌
+                if(pos[i]==10) mod[i]=1;
+                else if(pos[i]==20) mod[i]=2;
+                else if(pos[i]==30 && mod[i]!=1) mod[i]=3;
+                dfs(nindex+1,score,pos,mod); //다음 이동으로 넘어가기
 
-            pos[i]=tmp;
-            score[i]+=pos[i];
-            if(pos[i]==10) mod[i]=1;
-            else if(pos[i]==20) mod[i]=2;
-            else if(pos[i]==30) mod[i]=3;
-            System.out.println(nindex);
-            print(score, pos,mod);
-
-            dfs(nindex+1,score,pos,mod);
-
-            mod[i]=premod;
-            pos[i]=pre;
-            score[i]-=tmp;
+                //원상복구
+                mod[i]=premod;
+                if(pos[i]!=40) score[i]-=pos[i];
+                pos[i]=pre;
+            }
         }
     }
-    static int move(int n, int idx,int now,int[]mod, int pre){
+    static int move(int n, int nowMod, int nowPos){
         int index;
-        int ret=0;
-        if(now==40) return 0;
-        switch(mod[idx]){
+        int nextPos=0;
+
+        //System.out.println(n+" "+nowMod+" "+nowPos);
+        switch(nowMod){
             //2씩 증가
             case 0:
-                ret=pre+2*n;
+                nextPos=nowPos+2*n;
+                if(nextPos>40) nextPos=40;
                 break;
             //10~40
             case 1:
-                index=from10.indexOf(pre);
+                index=from10.indexOf(nowPos);
                 n+=index;
-                if(n>7) n=7;
-                ret=from10.get(n);
+                if(n>7) n=7; //도착보다 넘어서면 도착으로 처리
+                nextPos=from10.get(n);
                 break;
             //20~40
             case 2:
-                index=from20.indexOf(pre);
+                index=from20.indexOf(nowPos);
                 n+=index;
                 if(n>6) n=6;
-                ret=from20.get(n);
+                nextPos=from20.get(n);
                 break;
             //30~40
             case 3:
-                index=from30.indexOf(pre);
+                index=from30.indexOf(nowPos);
                 n+=index;
                 if(n>7) n=7;
-                ret=from30.get(n);
+                nextPos=from30.get(n);
                 break;
         }
-        return ret;
+        return nextPos;
     }static void print(int[]score, int[]pos, int[]mod){
         for(int i=0;i<4;i++){
             System.out.print(mod[i]+" ");
